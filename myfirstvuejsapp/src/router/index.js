4236fad301f277abd5695c84380fb5f1554e382f
Dev <d.gurung@deakin.edu.au>
2023-08-07 18:54:13 +1000

    first commit

---
+++ myfirstvuejsapp/src/router/index.js
@@ -0,0 +1,31 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import ExampleView from '../views/ExampleView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/example',
+      name: 'example',
+      // component: () => import('../views/ExampleView.vue')
+      component: ExampleView
+      // route level code-splitting
+    }  
+  ]
+})
+
+export default router
---
+++ myfirstvuejsapp/src/views/ExampleView.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+
+</script>
+
+
+<template>
+
+
+<div>
+    This is example page.
+</div>
+
+
+</template>
+
+
+<style scoped>
+
+
+div {
+    width: 100%;
+    height: 500px;
+    background-color: bisque;
+}
+
+</style>
