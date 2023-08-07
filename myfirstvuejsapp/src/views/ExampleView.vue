6c6905dfe2d88d2ce4077331289912b4d8383c35
Dev <d.gurung@deakin.edu.au>
2023-08-07 19:36:41 +1000

    Shopping List

--- myfirstvuejsapp/src/views/ExampleView.vue
+++ myfirstvuejsapp/src/views/ExampleView.vue
@@ -1,27 +1,103 @@
 <script setup>
 import { ref } from 'vue'
 
+const itemId = ref(0)
+const shoppingitem = ref('')
+const shoppinglist = ref([])
+const shopped = ref(false)
+
+function addItem() {
+    shoppinglist.value.push({
+        id: itemId.value++,
+        item: shoppingitem.value,
+        status: shopped.value
+    })
+    shoppingitem.value = ''
+}
+
+function removeItem(item) {
+    shoppinglist.value = shoppinglist.value.filter(
+        (t) => t !== item)
+}
+
 </script>
 
 
 <template>
+    <div>
+    <h1>My Shopping List</h1>
+    <input type="text" v-model="shoppingitem">
+    <button class="buttonstyle" @click="addItem">Add Item</button>
+    <ol>
+
+        <li v-for="i in shoppinglist" :key="i.id">
+
+        {{ i.item }}
+
+        <button @click="removeItem(i)">X</button>
+            
+
+        </li>
+
+    </ol>
 
 
-<div>
-    This is example page.
 </div>
+
 
 
 </template>
 
 
 <style scoped>
+.divstyle {
+  width: 100%;
+  height: 400px;
+  background-color: aquamarine;
+  padding: 70px;
+}
+.done {
+    text-decoration: line-through;
+}
+input[type="text"] {
+width: 80%;
+padding: 10px;
+font-size: 20px;
+align-content: center;
+}
 
+input[type="checkbox"] {
+font-size: 20px;
+margin-right: 1px;
+}
+
+.buttonstyle {
+width: 80%;
+background-color: #4caf50;
+color: white;
+padding: 14px 20px;
+margin: 8px 0;
+border: none;
+border-radius: 4px;
+cursor: pointer;
+}
 
 div {
-    width: 100%;
-    height: 500px;
-    background-color: bisque;
+border-radius: 5px;
+background-color: #f2f2f2;
+padding: 20px;
+}
+h1 {
+width: 100%;
+text-align: center;
+}
+
+span {
+padding: 20px;
+font-size: 20px;
+}
+li {
+width: 100%;
 }
 
 </style>
